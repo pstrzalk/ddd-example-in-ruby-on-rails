99825206f17ccb3f782249b392798b697f7303fb
Paweł Strzałkowski <p.strzalkowski@visuality.pl>
2023-04-16 20:19:12 +0200

    Adjust Authorization::Application::Api.identity_in_role? and fix seeds
    The previous change was introduced hastly and cointained a bug in seeds.
    The (simulated) logic for checking whether an identity is in role has been adjusted.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,20 +2,22 @@ def print_header(label)
   puts "\n========================\n== #{label}"
 end
 
-contract_repository = CooperationNegotiation::Infrastructure::DbContractRepository.new
+contract_repository = CooperationNegotiation::Domain::ContractRepository.get
 
-client_id = '00000000-0000-0000-0000-000000000001'
-CooperationNegotiation::Application::Services::PrepareDraftContract.new.call(client_id: client_id)
+admin_identity = '00000000-0000-0000-0000-000000000001'
+
+client_id = '00000000-0000-0000-0000-000000000007'
+CooperationNegotiation::Application::Services::PrepareDraftContract.new.call(identity: admin_identity, client_id: client_id)
 contract = contract_repository.of_client_id(client_id)
 CooperationNegotiation::Application::Services::ModifyContractText.new.call(contract_id: contract.id, text: 'foo foo')
 
-client_id = '00000000-0000-0000-0000-000000000002'
-CooperationNegotiation::Application::Services::PrepareDraftContract.new.call(client_id: client_id)
+client_id = '00000000-0000-0000-0000-000000000008'
+CooperationNegotiation::Application::Services::PrepareDraftContract.new.call(identity: admin_identity, client_id: client_id)
 contract = contract_repository.of_client_id(client_id)
 CooperationNegotiation::Application::Services::SignContractByClient.new.call(contract_id: contract.id)
 
-client_id = '00000000-0000-0000-0000-000000000003'
-CooperationNegotiation::Application::Services::PrepareDraftContract.new.call(client_id: client_id)
+client_id = '00000000-0000-0000-0000-000000000009'
+CooperationNegotiation::Application::Services::PrepareDraftContract.new.call(identity: admin_identity, client_id: client_id)
 contract = contract_repository.of_client_id(client_id)
 CooperationNegotiation::Application::Services::ModifyContractText.new.call(contract_id: contract.id, text: 'The correct one')
 CooperationNegotiation::Application::Services::SignContractByClient.new.call(contract_id: contract.id)
--- spec/modules/authorization/application/api_spec.rb
+++ spec/modules/authorization/application/api_spec.rb
@@ -8,7 +8,7 @@ module Authorization
           let(:identity) { '00000000-0000-0000-0000-000000000001' }
 
           it { expect(described_class.identity_in_role?(identity, 'admin')).to be true }
-          it { expect(described_class.identity_in_role?(identity, 'client')).to be false }
+          it { expect(described_class.identity_in_role?(identity, 'client')).to be true }
           it { expect(described_class.identity_in_role?(identity, 'foo')).to be false }
         end
 
@@ -20,12 +20,12 @@ module Authorization
           it { expect(described_class.identity_in_role?(identity, 'foo')).to be false }
         end
 
-        context 'when checking a random identity' do
-          let(:identity) { '11112222-0000-0000-0000-000000000000' }
+        context 'when checking a non-uuid identity' do
+          let(:identity) { 'foo' }
 
           it { expect(described_class.identity_in_role?(identity, 'client')).to be false }
           it { expect(described_class.identity_in_role?(identity, 'admin')).to be false }
-          it { expect(described_class.identity_in_role?(identity, 'foo')).to be false }
+          it { expect(described_class.identity_in_role?(identity, 'bar')).to be false }
         end
       end
     end
